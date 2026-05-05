38578a110319c998fc7c632ca2204c3e79d2e903
manishkumar179 <mk0437271@gmail.com>
2026-05-06 01:30:18 +0530

    add: login controller and login routes

--- insta-clone/src/controllers/auth.controllers.js
+++ insta-clone/src/controllers/auth.controllers.js
@@ -42,6 +42,49 @@ let registerController = async (req, res) => {
   }
 };
 
+
+let loginController = async (req,res)=>{
+  try {
+    let {username , password} = req.body
+    if(!username || !password){
+      return res.status(400).json({
+        message:"All fields are required"
+      })
+    }
+
+    let isExist = await UserModel.findOne({username});
+    if(!isExist){
+      return res.status(404).json({
+        message:"User not found"
+      })
+    }
+
+    let checkPass = isExist.comparePassword(password);
+    if(!checkPass){
+      return res.status(401).json({
+        message:"Invalid Credentials"
+      })
+    }
+
+    let token = isExist.generateJWT();
+
+    res.cookie("token" , token);
+
+
+    return res.status(200).json({
+      message: "User Loggedin successfully",
+      user: isExist,
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      message:"Interval server error"
+    })
+  }
+}
+
+
 module.exports = {
-    registerController
+    registerController,
+    loginController
 }
--- insta-clone/src/routes/auth.routes.js
+++ insta-clone/src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 let express = require("express")
-const { registerController } = require("../controllers/auth.controllers")
+const { registerController, loginController } = require("../controllers/auth.controllers")
 
 let router = express.Router()
 
-router.post("/register" ,registerController )
+router.post("/register" ,registerController );
+router.post("/login" , loginController)
 
 
 module.exports = router
